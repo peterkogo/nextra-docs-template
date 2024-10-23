71d046bad32d5a40b26f38075557d9af0c640dd7
peterkogo <peter@xyflow.com>
2024-10-23 16:09:53 +0200

    updated dependencies & fixed migration errors & use component inside head

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,34 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
 
 const config: DocsThemeConfig = {
   logo: <span>My Project</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/shuding/nextra-docs-template",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Nextra Docs Template",
   },
-}
+  head() {
+    const { pathname } = useRouter();
+    const { frontMatter } = useConfig();
 
-export default config
+    const title = frontMatter.title ?? pathname;
+
+    return (
+      <>
+        <title>{title}</title>
+        <Head title={title} />
+      </>
+    );
+  },
+};
+
+const Head = ({ title }) => <title>{title}</title>;
+
+export default config;
